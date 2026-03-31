30e3496e103a608fb2d74d8e792fd2e84315450c
ab4041 <138356134+ab4041@users.noreply.github.com>
2026-03-31 20:19:48 +0530

    feat: implement UC3 Generic QuantityLength class using enum and DRY principle

--- src/main/java/com/quantity/QuantityMeasurementApp.java
+++ src/main/java/com/quantity/QuantityMeasurementApp.java
@@ -1,37 +1,44 @@
 package com.quantity;
 
+import java.util.Objects;
+
 public class QuantityMeasurementApp {
 
-    public static class Feet {
+    // ENUM for Units
+    public enum LengthUnit {
 
-        private final double value;
+        FEET(1.0),
+        INCH(1.0 / 12.0);
 
-        public Feet(double value) {
-            this.value = value;
+        private final double conversionFactor;
+
+        LengthUnit(double conversionFactor) {
+            this.conversionFactor = conversionFactor;
         }
 
-        @Override
-        public boolean equals(Object obj) {
-
-            if (this == obj)
-                return true;
-
-            if (obj == null || getClass() != obj.getClass())
-                return false;
-
-            Feet feet = (Feet) obj;
-
-            return Double.compare(feet.value, value) == 0;
+        public double toFeet(double value) {
+            return value * conversionFactor;
         }
     }
 
-
-    public static class Inches {
+    // Generic Quantity Class
+    public static class QuantityLength {
 
         private final double value;
+        private final LengthUnit unit;
 
-        public Inches(double value) {
+        public QuantityLength(double value, LengthUnit unit) {
+
+            if (unit == null) {
+                throw new IllegalArgumentException("Unit cannot be null");
+            }
+
             this.value = value;
+            this.unit = unit;
+        }
+
+        private double toFeet() {
+            return unit.toFeet(value);
         }
 
         @Override
@@ -43,9 +50,14 @@ public class QuantityMeasurementApp {
             if (obj == null || getClass() != obj.getClass())
                 return false;
 
-            Inches inches = (Inches) obj;
+            QuantityLength other = (QuantityLength) obj;
 
-            return Double.compare(inches.value, value) == 0;
+            return Double.compare(this.toFeet(), other.toFeet()) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(toFeet());
         }
     }
 }
